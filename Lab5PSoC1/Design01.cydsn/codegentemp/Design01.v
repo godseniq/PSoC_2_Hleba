2581c5afc4f7c13f1932fb18f6c890f2a34c510d
godseniq <161821422+godseniq@users.noreply.github.com>
2025-10-08 17:19:34 +0300

    Оновлено проект

--- Lab5PSoC1/Design01.cydsn/codegentemp/Design01.v
+++ Lab5PSoC1/Design01.cydsn/codegentemp/Design01.v
@@ -1,6 +1,6 @@
 // ======================================================================
 // Design01.v generated from TopDesign.cysch
-// 10/08/2025 at 15:45
+// 10/08/2025 at 17:14
 // This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 // ======================================================================
 
